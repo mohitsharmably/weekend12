b9678f5d808f504dc6310600a4626649684d2d90
windows <windows@microsoft.com>
2023-07-22 13:22:58 +0530

    create project

---
+++ src/Myhome.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import "./style.css";
+
+function Myhome() {
+  return (
+    <div>Myhom sm,d skjd gfjhs gfhjsdgfh gsdh fghsgduygsdufgusde</div>
+  )
+}
+export default Myhome
+
+
+export function Myabout()
+{
+  return(
+    <Fragment>
+        <h1>welcome to name export function</h1>
+        <p className='first'>this is paragrph</p>
+    </Fragment>
+  )
+}
+
+
+
+
+
